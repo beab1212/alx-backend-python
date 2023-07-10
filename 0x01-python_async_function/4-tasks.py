e918380fe544618266d9aabda3cde3076eef9593
Mr-robot <hackingtest017@gmail.com>
2023-07-10 15:11:28 -0400

    anotation fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Tasks """
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """  Create task_wait_random task n times.
         Args:
             n (int): no of times for task
