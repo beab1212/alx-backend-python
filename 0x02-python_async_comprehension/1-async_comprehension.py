c3c43fd37cf9f91bffca198980cbb7569575859b
Mr-robot <hackingtest017@gmail.com>
2023-07-11 14:56:36 -0400

    init

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+""" Async Comprehensions """
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+        Args:
+        Return:
+            List of float numbers generated from async_generator func
+    """
+    return [i async for i in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+""" Run time for four parallel comprehensions """
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+        Args:
+        Return:
+            float represent (seconds) that taken
+            to run the async_comprehension 4 times
+    """
+    s = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    return time.perf_counter() - s
