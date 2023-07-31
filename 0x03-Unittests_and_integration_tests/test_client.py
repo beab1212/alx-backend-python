bc2d3b882a9a2a5e7b8e9c6b96db9e1f5fe5273e
Mr-robot <hackingtest017@gmail.com>
2023-07-31 16:01:03 -0400

    fix

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""A module for testing the client module.
+"""
+import unittest
+from typing import Dict
+from requests import HTTPError
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+from parameterized import parameterized, parameterized_class
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Class for Integration test of fixtures """
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Sets up class fixtures before running tests."""
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """ Integration test: public repos"""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """ Integration test for public repos with License """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """"
+        A class method called after tests in an individual class have run
+        """
+        cls.get_patcher.stop()
